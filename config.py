16264e0c42a3946d0437617dbe42f559e01352c0
josh <josh3255@kangwon.ac.kr>
2022-01-17 12:57:04 +0900

    latest update
    add train code and modify hyper parameters

--- config.py
+++ config.py
@@ -4,13 +4,14 @@ def get_args_parser():
     parser = argparse.ArgumentParser('GAN Parameters', add_help=False)
 
     # train and valid params
-    parser.add_argument('--lr', default=4e-4, type=float)
+    parser.add_argument('--lr', default=0.0002, type=float)
+    parser.add_argument('--beta1', default=0.5, type=float)
     parser.add_argument('--epochs', default=6000, type=int)
     parser.add_argument('--weight_decay', default=5e-4, type=float)
     parser.add_argument("--batch_size", type=int, default=512, help="train batch size")
     parser.add_argument("--num_workers", type=int, default=8, help="number of workers")
 
-    parser.add_argument('--train_ratio', default=2, type=int)
+    parser.add_argument('--train_ratio', default=1, type=int)
     parser.add_argument('--save_term', default=50, type=int)
 
     parser.add_argument('--train_root_path', default='/data/mnist')
--- models.py
+++ models.py
@@ -23,13 +23,12 @@ class Generator(nn.Module):
             nn.BatchNorm2d(32),
             nn.ReLU(),
 
-            nn.ConvTranspose2d(32, 3, kernel_size=4, stride=2, padding=1),
+            nn.ConvTranspose2d(32, 1, kernel_size=4, stride=2, padding=1),
             nn.Tanh()
         )
 
     def forward(self, z):
         output = self.model(z)
-        print(output.shape)
         return output
 
 class Discriminator(nn.Module):
@@ -38,7 +37,7 @@ class Discriminator(nn.Module):
         self.args = args
 
         self.model = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=4, stride=2, padding=1),
+            nn.Conv2d(1, 32, kernel_size=4, stride=2, padding=1),
             nn.LeakyReLU(0.2, inplace=True),
             
             nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),
@@ -56,12 +55,13 @@ class Discriminator(nn.Module):
     def forward(self, x):
         output = self.model(x)
         output = output.view(-1, 1).squeeze(1)
-        print(output.shape)
         return output
 
 def main(args):
     g = Generator(args)
+    print(torch.randn((4, args.latent_dimension)).shape)
     tmp = torch.randn((4, args.latent_dimension)).view(-1, args.latent_dimension, 1, 1)
+    print(tmp.shape)
     g_out = g(tmp)
     d = Discriminator(args)
     d_out = d(g_out)
